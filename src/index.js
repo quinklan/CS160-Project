9eec322b0e3a0ddae129f7ecc6790e1c9d07022b
Quinlan <quinlanhnguyen@gmail.com>
2022-03-20 01:54:27 -0700

    createTheme changed to createMuiTheme in index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Routing from './Routing';
 import { withRouter } from 'react-router-dom';
-import { ThemeProvider } from '@material-ui/styles';
-import { createTheme } from '@material-ui/core';
+// import { ThemeProvider } from '@material-ui/styles';
+// import { createTheme } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import createTypography from '@material-ui/core/styles/';
 import createPalette from '@material-ui/core/styles/';
 
 import {checkIfUserIsSignedIn} from './ApiFunctions/User';
 import { teal } from '@material-ui/core/colors';
 
-const theme = createTheme({
+const theme = createMuiTheme({
   palette: {
     primary: {
       main: '#f69e20'
