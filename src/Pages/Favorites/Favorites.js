b5339ce3b69817b3853b580dd25772578400e6c3
Sohan Bhat <77870902+sohan-bhat-git@users.noreply.github.com>
2022-03-30 15:55:31 -0700

    Add favorite icon and review prop

--- src/Pages/Favorites/Favorites.js
+++ src/Pages/Favorites/Favorites.js
@@ -13,8 +13,10 @@ export default function Favorites(props) {
     
     //props image, restaurantTitle, address, rating, tags
     <React.Fragment>
+        <div style = {{width: '75%'}}>
         <RestaurantListing image = "https://media-cdn.tripadvisor.com/media/photo-s/11/4a/b8/61/a-burger-for-every-lifestyle.jpg" restaurantTitle="The Counter" address='20800 Stevens Creek Blvd' rating= {2.5} tags={['Bars', 'Burgers', 'Sandwiches']} />
-        <RestaurantListing image = "https://media-cdn.tripadvisor.com/media/photo-s/11/4a/b8/61/a-burger-for-every-lifestyle.jpg" restaurantTitle="The Counter" address='20800 Stevens Creek Blvd' rating= {2.5} tags={['Bars', 'Burgers', 'Sandwiches']} />           
+        <RestaurantListing image = "https://s3-media2.fl.yelpcdn.com/bphoto/Q7ErHze2LxTdmuKSYq94jQ/o.jpg" restaurantTitle="The Counter" description='This food here is great! However, the customer service here is terrible. I had to wait an HOUR before I got my food.' rating= {2.5} tags={['Bars', 'Burgers', 'Sandwiches']} review/>           
+        </div>
     </React.Fragment>
   );
 }
